9e25e1aa68d46ed4ed1c270ea1dd6ad6f40072ce
Evgeni1494 <yoni1@hotmail.fr>
2023-03-17 15:15:40 +0100

    Jira_test_commit

---
+++ Jira_test.py
@@ -0,0 +1,21 @@
+
+def super_code():
+    super_code_de_mort = 0
+    for i in super_code_de_mort:
+        if super_code_de_mort > 0:
+            i +=1
+        else:
+            i -=1
+        return i
+        
+
+
+
+
+
+
+
+
+
+
+
